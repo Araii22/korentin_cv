4fa16d0db2d6c92e4752acbaa0ff5d7dd307da4e
allan-lat <allan@toketa.com>
2018-07-28 00:22:20 +0200

    fichier Brut

---
+++ CV.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr"> 
+	<head>
+		<title> CV  Korentin Chaume </title>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="CV.css">
+	</head> 
+	<body class="background">
+		
+		<header>
+		<h1> CV de Korentin Chaume	</h1>
+		      <ul id='nav'>
+						<li id='nav'> <a href='CV'> Acceuil </a></li>
+						<li id='nav'><a href='Formations'> Formations </a></li>
+						<li id='nav'><a href='three'> Divers </a></li>
+						<li id='nav'><a href='four'> Contact </a></li>
+			  </ul>
+		</header>
+		
+		<div class="Acceuil">
+		<p> Actuellement étudiant en deuxième année de BTS Système Numérique avec le CNED, 
+		je suis à la recherche d'un stage d'un mois pour septembre. </p>
+		</div>
+	</body>
+</html>
---
+++ Formations.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr"> 
+	<head>
+		<title> CV de Korentin Chaume </title>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="CV.css">
+	</head> 
+	<header>
+		<?php echo "<h1> CV de Korentin Chaume	</h1>"; 
+		      echo "<ul id='nav'>
+						<li id='nav'> <a href='CV'> Acceuil </a></li>
+						<li id='nav'><a href='Formations'> Formations </a></li>
+						<li id='nav'><a href='Langues'> Langues </a></li>
+						<li id='nav'><a href='three'> Divers </a></li>
+						<li id='nav'><a href='four'> Contact </a></li>"; ?>
+	</header>
+	<body> 
+		<div class="Formations">
+			<?php echo "<table> <tr> <td><h3 id='gauche'> Première année de </br> BTS Système Numérique </h3></td> 
+						<td><h3 id='centre'> CNED Grenoble </h3></td>
+						<td><h3 id='droite'> 2017-2018 </h3></td></tr></table>";?>
+		<div class='description'>	
+			<?php echo"<p> Tronc commun (Algorithmie, C/C++/C Arduino, Protocoles réseaux),</br> 
+			Sciences Physiques, Mathématiques, Anglais, Culture Général et Expression</p> </br>
+			<p id='lien'>Lien vers la formation : <a href='http://www.cned.fr/inscription/33840DIX'> BTS SN CNED </a> </p></br>
+			<iframe id='carte' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2811.7313322580517!2d5.774410615953726!3d45.192534579098776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478af528bb13261b%3A0x506e4ef105336c5a!2sCNED+-+Site+de+Grenoble!5e0!3m2!1sfr!2sfr!4v1532606895508' ></iframe> </br> </br>";?>		
+		</div></div>
+		
+		<div clas="Formations">
+			<?php echo"<table> <tr> <td><h3> Terminale Scientifique spécialité Mathématiques</h3></td> 
+						<td><h3> CNED Rennes </h3></td>
+						<td><h3> 2015-2017 </h3></td></tr></table>";?>
+		<div class="description">
+			<?php echo"<p> Mathématiques, Anglais, Italien, Français, Physique-Chimie, SVT</p> 
+			<p id='lien'> Lien vers la formation: <a href='http://www.cned.fr/inscription/7TERGDI1'> Terminale CNED </a> </p></br>
+			<iframe id='carte'src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2663.2615383899865!2d-1.6319700839675007!3d48.12447947922238!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480edec1691d9f37%3A0xf183e5c71a2a0dc3!2sCNED+-+Site+de+Rennes!5e0!3m2!1sfr!2sfr!4v1532608053158'></iframe> </br> </br>";?>
+		</div> </div>
+	
+		<div class="Formations">
+			<?php echo"<table> <tr> <td><h3> Première Scientifique </h3></td> 
+						<td><h3> CNED Rennes </h3></td>
+						<td><h3> 2014-2015 </h3></td></tr></table>";?>
+		<div class="description">
+			<?php echo"Mathématiques, Anglais, Italien, Français, Physique-Chimie, SVT</p> 
+			<p id='lien'> Lien vers la formation: <a href='http://www.cned.fr/inscription/7PREGDI1'> Première CNED </a> </p> </br>
+			<iframe id='carte'src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2663.2615383899865!2d-1.6319700839675007!3d48.12447947922238!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480edec1691d9f37%3A0xf183e5c71a2a0dc3!2sCNED+-+Site+de+Rennes!5e0!3m2!1sfr!2sfr!4v1532608053158'></iframe> </br> </br>";?>
+	</body> 
+</html>
