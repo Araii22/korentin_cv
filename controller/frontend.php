e29e18df7b3236cbb63658b9251ca8de77e273a4
allan-lat <allan@toketa.com>
2018-07-29 13:15:15 +0200

    recreation de frontend

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,8 +1,5 @@
 <?php 
-
-
-
-//affichage des pages
+//ici affichage des pages
 function postPage()
 {
 	
@@ -10,25 +7,29 @@ function postPage()
 		{
 			require('views/frontend/accueil.php');
 		}
-		elseif ($_GET['page'] == 'formations') 
+		elseif ($_GET['page'] == 'Formations') 
 		{
-			require('views/frontend/formations.php');
+			require('views/frontend/formation.php');
 		}
 		elseif ($_GET['page'] == 'divers') 
 		{
 			require('views/frontend/divers.php');
+		}
+		
+		elseif ($_GET['page'] == 'mentions-legales') 
+		{
+			require('views/frontend/mentions-legales.php');
 		}
 		else {
 			require('views/frontend/accueil.php');
 		}
 }
 
-
-//Page contact 
+//ici envoie d'email <br>
 function sendMail()
 {
-	// Ma clé privée
-	/*$secret = "6Le09F4UAAAAACJTdvLGrlnvpuujo16-_XIPMdV8";
+	/*// Ma clé privée
+	$secret = "6Le09F4UAAAAACJTdvLGrlnvpuujo16-_XIPMdV8";
 	// Paramètre renvoyé par le recaptcha
 	$response = $_POST['g-recaptcha-response'];
 	// On récupère l'IP de l'utilisateur
@@ -41,9 +42,9 @@ function sendMail()
 	
 	$decode = json_decode(file_get_contents($api_url), true);
 	
-	if ($decode['success'] == true) { */
+	if ($decode['success'] == true) {*/
 		// C'est un humain
-$adress = "allan@toketa.com";
+		$adress = "webmaster@guingamp-business.com";
 
 $TO = $adress;
 
@@ -55,47 +56,33 @@ $head .="Return-Path:<".$adress.">\n";
 $head .="Content-Type: text/plain; charset=utf-8\n";
 
 //ensuite on passe les données dans des variables
-$civilite = $_POST['civilite'];
 $nom = $_POST['nom'];
 $prenom = $_POST['prenom'];
-$tel = $_POST['telephone'];
 $email = $_POST['email'];
-$sujet_message = $_POST['sujet'];
 $message = $_POST['message'];
-$adhesion = $_POST['adhesion'];
 	
 //Maintenant on fait les trucs relou il faut tout sécurisé
 	
-    $civilite=htmlspecialchars($_POST['civilite']);
-    $civilite=trim($civilite);
-	
+   
     $nom=htmlspecialchars($_POST['nom']);
     $nom=trim($nom);
 	
     $prenom=htmlspecialchars($_POST['prenom']);
     $prenom=trim($prenom);
 	
-	$entreprise=htmlspecialchars($_POST['entreprise']);
-    $entreprise=trim($entreprise);
 	
-	$codepostal=htmlspecialchars($_POST['codepostal']);
-    $codepostal=trim($codepostal);
 	
-    $tel=htmlspecialchars($_POST['tel']);
-    $tel=trim($tel);
 	
     $email=htmlspecialchars($_POST['email_adress']);
     $email=trim($email);
 	
-	$sujet_message=htmlspecialchars($_POST['sujet']);
-    $sujet_message=trim($sujet_message);
 	
 	$message=htmlspecialchars($_POST['message']);
     $message=trim($message);
 	
 	
 //là on définit le sujet du message qui apparaitra dans le mail
-$sujet ="Korentin Web site ".$sujet_messsage."\n";
+$sujet ="CV Korentin: ".$sujet_messsage."\n";
 
 	
 //Ensuite on écrit le message que l'on souhaite
@@ -103,30 +90,32 @@ $sujet ="Korentin Web site ".$sujet_messsage."\n";
 	
 $informations ="Bonjour,
 
-".$civilite." ".$prenom." ".$nom." travaillant chez ".$entreprise." souhaite des informations au sujet de ".$sujet_message."
+".$prenom." ".$nom." souhaite te contacter
+
 
 
 Son message :
 
 ".$message."
 
-Vous pouvez prendre contact au ".$tel." ou par mail ".$email."
+Vous pouvez prendre contact  par mail ".$email."
 
 Allez hop ! au boulot !
-".$codepostal."
+
 ";
 
 //maintenant on envoie le mail !
 $res = mail($TO, $sujet, $informations, $head);
 	
 $message_error = "Votre message a été envoyé";
+$message_color = "success";
 require('views/frontend/contact.php');
-	/*}
+	}
 	
-	else {
+	/*else {
 		// C'est un robot ou le code de vérification est incorrecte
 		$message_error = "Valider le Captcha";
 		require('views/frontend/contact.php');
-	}
-*/
-}
+	}*/
+
+
