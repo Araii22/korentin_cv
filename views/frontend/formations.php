7b81fbace9ca661a19315a1163ff6ae0513b38dd
allan-lat <allan@toketa.com>
2018-07-29 12:40:47 +0200

    Creation page formations ok

--- views/frontend/formations.php
+++ views/frontend/formations.php
@@ -1,10 +1,94 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Document sans titre</title>
-</head>
+<?php $title = 'Formations - Korentin Chaume '; ?>
 
-<body>
-</body>
-</html>
+<?php ob_start(); ?>
+
+ <div class="row pt-5 mb-5 pb-5">
+		  
+		  	<div class="col-12 mt-5">
+				  <div class="article mt-5">
+					 <!-- mise en page du header-->
+					<div class="row header-post">
+						<div class="col-12 col-md-4">
+							<p>Première année de BTS Système Numérique </p>
+						</div>
+						<div class="col-12 col-md-4">
+							<p>CNED Grenoble </p>
+						</div>
+						<div class="col-12 col-md-4">
+							<p>2017-2018</p>
+						</div>
+					  </div>
+					  <hr>
+					  <!-- mise en page contenu -->
+					 <div class="row pt-4">
+					 	<div class="col-12 col-md-8">
+						 	<p>Tronc commun (Algorithmie, C/C++/C Arduino, Protocoles réseaux),
+Sciences Physiques, Mathématiques, Anglais, Culture Général et Expression</p>
+						 </div> 
+					 	<div class="col-12 col-md-4">
+							<img src="public/images/400X200.gif" class="img-fluid" alt=""/>
+						</div> 
+					 </div> 
+				  </div>
+			</div>
+		  	<div class="col-12 mt-5">
+				  <div class="article mt-5">
+					 <!-- mise en page du header-->
+					<div class="row header-post">
+						<div class="col-12 col-md-4">
+							<p>Première année de BTS Système Numérique </p>
+						</div>
+						<div class="col-12 col-md-4">
+							<p>CNED Grenoble </p>
+						</div>
+						<div class="col-12 col-md-4">
+							<p>2017-2018</p>
+						</div>
+					  </div>
+					  <hr>
+					  <!-- mise en page contenu -->
+					 <div class="row pt-4">
+					 	<div class="col-12 col-md-8">
+						 	<p>Tronc commun (Algorithmie, C/C++/C Arduino, Protocoles réseaux),
+Sciences Physiques, Mathématiques, Anglais, Culture Général et Expression</p>
+						 </div> 
+					 	<div class="col-12 col-md-4">
+							<img src="public/images/400X200.gif" class="img-fluid" alt=""/>
+						</div> 
+					 </div> 
+				  </div>
+			</div>
+		  	<div class="col-12 mt-5">
+				  <div class="article mt-5">
+					 <!-- mise en page du header-->
+					<div class="row header-post">
+						<div class="col-12 col-md-4">
+							<p>Première année de BTS Système Numérique </p>
+						</div>
+						<div class="col-12 col-md-4">
+							<p>CNED Grenoble </p>
+						</div>
+						<div class="col-12 col-md-4">
+							<p>2017-2018</p>
+						</div>
+					  </div>
+					  <hr>
+					  <!-- mise en page contenu -->
+					 <div class="row pt-4">
+					 	<div class="col-12 col-md-8">
+						 	<p>Tronc commun (Algorithmie, C/C++/C Arduino, Protocoles réseaux),
+Sciences Physiques, Mathématiques, Anglais, Culture Général et Expression</p>
+						 </div> 
+					 	<div class="col-12 col-md-4">
+							<img src="public/images/400X200.gif" class="img-fluid" alt=""/>
+						</div> 
+					 </div> 
+				  </div>
+			</div>
+		  	
+		  </div>
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('views/frontend/template/pages.php'); ?>
