c24345c57887edbeb092c33112d99da90cc2f72c
allan-lat <allan@toketa.com>
2018-07-29 12:39:11 +0200

    page accueil ok

--- views/frontend/accueil.php
+++ views/frontend/accueil.php
@@ -1,10 +1,18 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Document sans titre</title>
-</head>
+<?php $title = 'Accueil - Korentin Chaume '; ?>
 
-<body>
-</body>
-</html>
+<?php ob_start(); ?>
+
+<div class="row pt-5 mb-5 pb-5">
+	<div class="col-1 col-sm-2"></div>
+		<div class="col-10 col-sm-8 mt-5">
+			  <div class="article mt-5">
+				<p>Actuellement étudiant en deuxième année de BTS Système Numérique avec le CNED, je suis à la recherche d'un stage d'un mois pour septembre..  </p>
+			  </div>
+		</div>
+	<div class="col-1 col-sm-2"></div>
+</div>
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('views/frontend/template/pages.php'); ?>
